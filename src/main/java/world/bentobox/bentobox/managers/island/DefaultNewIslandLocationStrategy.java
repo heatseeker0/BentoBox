25ae74db3304b9e068e3b3a3c0ad50de62fee047
Catalin Ionescu <cionescu+github@gmail.com>
2020-06-04 22:19:06 +0300

    Modified new island location detection to use Paper async chunk loading.

--- src/main/java/world/bentobox/bentobox/managers/island/DefaultNewIslandLocationStrategy.java
+++ src/main/java/world/bentobox/bentobox/managers/island/DefaultNewIslandLocationStrategy.java
@@ -1,14 +1,18 @@
 package world.bentobox.bentobox.managers.island;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
 import world.bentobox.bentobox.BentoBox;
@@ -16,6 +20,7 @@ import world.bentobox.bentobox.util.Util;
 
 /**
  * The default strategy for generating locations for island
+ * 
  * @author tastybento, leonardochaia
  * @since 1.8.0
  *
@@ -29,7 +34,9 @@ public class DefaultNewIslandLocationStrategy implements NewIslandLocationStrate
     protected static final Integer MAX_UNOWNED_ISLANDS = 20;
 
     protected enum Result {
-        ISLAND_FOUND, BLOCKS_IN_AREA, FREE
+        ISLAND_FOUND,
+        BLOCKS_IN_AREA,
+        FREE
     }
 
     protected BentoBox plugin = BentoBox.getInstance();
@@ -38,14 +45,15 @@ public class DefaultNewIslandLocationStrategy implements NewIslandLocationStrate
     public Location getNextLocation(World world) {
         Location last = plugin.getIslands().getLast(world);
         if (last == null) {
-            last = new Location(world,
-                    (double) plugin.getIWM().getIslandXOffset(world) + plugin.getIWM().getIslandStartX(world),
-                    plugin.getIWM().getIslandHeight(world),
-                    (double) plugin.getIWM().getIslandZOffset(world) + plugin.getIWM().getIslandStartZ(world));
+            last = new Location(world, (double) plugin.getIWM().getIslandXOffset(world) + plugin.getIWM().getIslandStartX(world), plugin.getIWM().getIslandHeight(
+                world), (double) plugin.getIWM().getIslandZOffset(world) + plugin.getIWM().getIslandStartZ(world));
         }
+
         // Find a free spot
         Map<Result, Integer> result = new EnumMap<>(Result.class);
         // Check center
+
+        // TODO: This does a lot of boxing and unboxing, potential for optimization here
         Result r = isIsland(last);
         while (!r.equals(Result.FREE) && result.getOrDefault(Result.BLOCKS_IN_AREA, 0) < MAX_UNOWNED_ISLANDS) {
             nextGridLocation(last);
@@ -57,8 +65,7 @@ public class DefaultNewIslandLocationStrategy implements NewIslandLocationStrate
             // We could not find a free spot within the limit required. It's likely this
             // world is not empty
             plugin.logError("Could not find a free spot for islands! Is this world empty?");
-            plugin.logError("Blocks around center locations: " + result.getOrDefault(Result.BLOCKS_IN_AREA, 0) + " max "
-                    + MAX_UNOWNED_ISLANDS);
+            plugin.logError("Blocks around center locations: " + result.getOrDefault(Result.BLOCKS_IN_AREA, 0) + " max " + MAX_UNOWNED_ISLANDS);
             plugin.logError("Known islands: " + result.getOrDefault(Result.ISLAND_FOUND, 0) + " max unlimited.");
             return null;
         }
@@ -73,13 +80,22 @@ public class DefaultNewIslandLocationStrategy implements NewIslandLocationStrate
      * @return Result enum if island found, null if blocks found, false if nothing found
      */
     protected Result isIsland(Location location) {
+        if (location == null) {
+            return Result.BLOCKS_IN_AREA;
+        }
+
         // Quick check
-        if (plugin.getIslands().getIslandAt(location).isPresent()) return Result.ISLAND_FOUND;
-        
+        if (plugin.getIslands().getIslandAt(location).isPresent()) {
+            return Result.ISLAND_FOUND;
+        }
+
         World world = location.getWorld();
+        if (world == null) {
+            return Result.BLOCKS_IN_AREA;
+        }
 
         // Check 4 corners
-        int dist = plugin.getIWM().getIslandDistance(location.getWorld());
+        int dist = plugin.getIWM().getIslandDistance(world);
         Set<Location> locs = new HashSet<>();
         locs.add(location);
 
@@ -88,25 +104,54 @@ public class DefaultNewIslandLocationStrategy implements NewIslandLocationStrate
         locs.add(new Location(world, location.getX() + dist - 1, 0, location.getZ() - dist));
         locs.add(new Location(world, location.getX() + dist - 1, 0, location.getZ() + dist - 1));
 
-        boolean generated = false;
         for (Location l : locs) {
             if (plugin.getIslands().getIslandAt(l).isPresent() || plugin.getIslandDeletionManager().inDeletion(l)) {
                 return Result.ISLAND_FOUND;
             }
-            if (Util.isChunkGenerated(l)) generated = true;
+
+            // If chunk has not been generated yet, then it's not occupied
+            if (!Util.isChunkGenerated(l)) {
+                return Result.FREE;
+            }
         }
-        // If chunk has not been generated yet, then it's not occupied
-        if (!generated) {
+
+        if (plugin.getIWM().isUseOwnGenerator(world)) {
             return Result.FREE;
         }
-        // Block check
-        if (!plugin.getIWM().isUseOwnGenerator(world) && Arrays.asList(BlockFace.values()).stream().anyMatch(bf -> 
-        !location.getBlock().getRelative(bf).isEmpty() && !location.getBlock().getRelative(bf).getType().equals(Material.WATER))) {
-            // Block found
-            plugin.getIslands().createIsland(location);
+
+        List<CompletableFuture<Result>> futures = new ArrayList<>();
+
+        int locX = location.getBlockX();
+        int locZ = location.getBlockZ();
+
+        for (BlockFace face : BlockFace.values()) {
+            // We need to load each async because different block faces can be part of different chunks. This ensures *all* chunk loads are async.
+            futures.add(Util.getChunkAtAsync(world, locX + face.getModX(), locZ + face.getModZ()).thenApply(r -> {
+                Block block = location.getBlock().getRelative(face);
+                if (!block.isEmpty() || (block.getType() != Material.WATER)) {
+                    // Block found. Mark this area as belonging to an island.
+                    plugin.getIslands().createIsland(location);
+                    return Result.BLOCKS_IN_AREA;
+                }
+
+                return Result.FREE;
+            }));
+        }
+
+        try {
+            // And finally calculate the outcome sync using short-circuit evaluation for even more speed gains
+            for (CompletableFuture<Result> future : futures) {
+                if (future.get() == Result.BLOCKS_IN_AREA) {
+                    return Result.BLOCKS_IN_AREA;
+                }
+            }
+
+            return Result.FREE;
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+
             return Result.BLOCKS_IN_AREA;
         }
-        return Result.FREE;
     }
 
     /**
